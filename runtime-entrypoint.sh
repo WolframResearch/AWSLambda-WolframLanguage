89ee6464cd65536b3670829cd381f912abae84de
Jesse Friedman <jessef@wolfram.com>
2021-02-02 19:49:14 -0500

    Remove debugging code from runtime-entrypoint.sh

--- runtime-entrypoint.sh
+++ runtime-entrypoint.sh
@@ -3,9 +3,6 @@
 # use, in order: existing _HANDLER; first argument; string "app"
 export _HANDLER="${_HANDLER:-${1:-app}}"
 
-whoami
-echo $HOME
-
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   exec /usr/local/bin/aws-lambda-rie /runtime-kernel-wrapper.sh $_HANDLER
 else
