f58e3f7d192b23f68fa89009f5cb8b30a5a738ed
Jesse Friedman <jessef@wolfram.com>
2021-04-28 19:36:41 -0400

    Modify build.sh and push.sh scripts

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,3 @@
 
 docker build . \
   -t wolframresearch/aws-lambda-wolframlanguage:latest
-
-docker build Demo \
-  -t 790731757232.dkr.ecr.us-east-1.amazonaws.com/aws-lambda-wolframlanguage-demo:latest
